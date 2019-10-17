5349bf6b782838fed630a9070d2db70a627fe626
Andrey Karazhev <andrey.karazhev@specific-group.by>
2019-10-17 11:09:24 +0300

    Implemented the basic MetaConfig class .

--- src/main/java/com/github/akarazhev/metaconfig/MetaConfig.java
+++ src/main/java/com/github/akarazhev/metaconfig/MetaConfig.java
@@ -1,4 +1,20 @@
 package com.github.akarazhev.metaconfig;
 
-public final class MetaConfig {
+import com.github.akarazhev.metaconfig.api.Config;
+
+import java.io.Closeable;
+import java.io.IOException;
+
+public final class MetaConfig implements Closeable {
+
+    public MetaConfig() {
+    }
+
+    public MetaConfig(Config config) {
+    }
+
+    @Override
+    public void close() throws IOException {
+        throw new RuntimeException("close is not implemented");
+    }
 }
