bc1ff9d21e94445b02b54edb3aa137b17d66f833
Andrey Karazhev <andrey.karazhev@specific-group.by>
2019-10-21 10:47:00 +0300

    Add in memory implementation.

--- src/main/java/com/github/akarazhev/metaconfig/api/ConfigRepositoryImpl.java
+++ src/main/java/com/github/akarazhev/metaconfig/api/ConfigRepositoryImpl.java
@@ -1,33 +1,37 @@
 package com.github.akarazhev.metaconfig.api;
 
 import javax.sql.DataSource;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Stream;
 
 final class ConfigRepositoryImpl implements ConfigRepository {
 
     private final DataSource dataSource;
+    private final Map<String, Config> inMemDataSource;
 
     ConfigRepositoryImpl(final DataSource dataSource) {
         this.dataSource = dataSource;
+        this.inMemDataSource = new ConcurrentHashMap<>();
     }
 
     @Override
     public Stream<Config> findByName(final String name) {
-        throw new RuntimeException("findByName is not implemented");
+        return Stream.of(inMemDataSource.get(name));
     }
 
     @Override
     public Stream<String> findNames() {
-        throw new RuntimeException("findNames is not implemented");
+        return inMemDataSource.keySet().stream();
     }
 
     @Override
     public Config saveAndFlush(final Config config) {
-        throw new RuntimeException("saveAndFlush is not implemented");
+        return inMemDataSource.put(config.getName(), config);
     }
 
     @Override
     public void delete(final String name) {
-        throw new RuntimeException("delete is not implemented");
+        inMemDataSource.remove(name);
     }
 }
