5ec1a8b7a12fbfdc4e18ff451cb2e525f372f133
Andrey Karazhev <andrey.karazhev@specific-group.by>
2019-11-13 13:51:13 +0300

    Added more samples for SQL scripts.

--- src/main/sql/insert_records.sql
+++ src/main/sql/insert_records.sql
@@ -4,3 +4,5 @@ INSERT INTO `CONFIGS` (`ID`, `NAME`, `DESCRIPTION`, `VERSION`, `UPDATED`) VALUES
 INSERT INTO `CONFIG_ATTRIBUTES` (`ID`, `CONFIG_ID`, `KEY`, `VALUE`) VALUES (1, 1, 'key_1', 'value_1');
 INSERT INTO `CONFIG_ATTRIBUTES` (`ID`, `CONFIG_ID`, `KEY`, `VALUE`) VALUES (2, 1, 'key_2', 'value_2');
 INSERT INTO `CONFIG_ATTRIBUTES` (`ID`, `CONFIG_ID`, `KEY`, `VALUE`) VALUES (3, 1, 'key_3', 'value_3');
+-- Insert a property
+INSERT INTO `PROPERTIES` (`ID`, `CONFIG_ID`, `NAME`, `CAPTION`, `DESCRIPTION`, `TYPE`, `VALUE`, `VERSION`) VALUES (1, 1, 'Simple Property', 'Simple Caption', 'Simple Description', 'BOOL', 'true', 1);
