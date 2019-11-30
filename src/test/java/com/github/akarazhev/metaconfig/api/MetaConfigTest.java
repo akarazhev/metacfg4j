7084cd5781ca4acefa8a4a281f0ee668869cb2a1
Andrey Karazhev <andrey.karazhev@specific-group.com>
2019-11-30 20:26:01 +0300

    Updated tests and a configuration.

--- src/test/java/com/github/akarazhev/metaconfig/api/MetaConfigTest.java
+++ src/test/java/com/github/akarazhev/metaconfig/api/MetaConfigTest.java
@@ -10,9 +10,83 @@
  * limitations under the License. */
 package com.github.akarazhev.metaconfig.api;
 
+import com.github.akarazhev.metaconfig.engine.db.DbServer;
+import com.github.akarazhev.metaconfig.engine.db.DbServers;
+import com.github.akarazhev.metaconfig.engine.db.pool.ConnectionPool;
+import com.github.akarazhev.metaconfig.engine.db.pool.ConnectionPools;
+import com.github.akarazhev.metaconfig.engine.web.WebClient;
+import com.github.akarazhev.metaconfig.engine.web.server.Server;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
+
+import java.io.IOException;
+import java.util.Arrays;
 
 @DisplayName("Meta config test")
 final class MetaConfigTest {
-    // todo
+    private static DbServer dbServer;
+    private static ConnectionPool connectionPool;
+    private static MetaConfig dbMetaConfig;
+    private static MetaConfig webMetaConfig;
+
+    @BeforeAll
+    static void beforeAll() throws Exception {
+        if (dbServer == null) {
+            dbServer = DbServers.newServer().start();
+        }
+
+        if (connectionPool == null) {
+            connectionPool = ConnectionPools.newPool();
+        }
+
+        if (dbMetaConfig == null) {
+            final Config webServer = new Config.Builder(Server.Settings.CONFIG_NAME,
+                    Arrays.asList(
+                            new Property.Builder(Server.Settings.HOSTNAME, "localhost").build(),
+                            new Property.Builder(Server.Settings.PORT, 8000).build(),
+                            new Property.Builder(Server.Settings.BACKLOG, 0).build(),
+                            new Property.Builder(Server.Settings.KEY_STORE_FILE, "./data/metacfg4j.keystore").build(),
+                            new Property.Builder(Server.Settings.ALIAS, "alias").build(),
+                            new Property.Builder(Server.Settings.STORE_PASSWORD, "password").build(),
+                            new Property.Builder(Server.Settings.KEY_PASSWORD, "password").build()))
+                    .build();
+            dbMetaConfig =
+                    new MetaConfig.Builder().webServer(webServer).dataSource(connectionPool.getDataSource()).build();
+        }
+
+        if (webMetaConfig == null) {
+            final Config webClient = new Config.Builder(WebClient.Settings.CONFIG_NAME,
+                    Arrays.asList(
+                            new Property.Builder(WebClient.Settings.URL, "https://localhost:8000/api/metacfg").build(),
+                            new Property.Builder(WebClient.Settings.ACCEPT_ALL_HOSTS, true).build()))
+                    .build();
+
+            dbMetaConfig =
+                    new MetaConfig.Builder().webClient(webClient).build();
+        }
+    }
+
+    @AfterAll
+    static void afterAll() throws IOException {
+        if (webMetaConfig != null) {
+            webMetaConfig.close();
+            webMetaConfig = null;
+        }
+
+        if (dbMetaConfig != null) {
+            dbMetaConfig.close();
+            dbMetaConfig = null;
+        }
+
+        if (connectionPool != null) {
+            connectionPool.close();
+            connectionPool = null;
+        }
+
+        if (dbServer != null) {
+            dbServer.stop();
+            dbServer = null;
+        }
+    }
 }
